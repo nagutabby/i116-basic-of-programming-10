e2938c4d4f6f94928833699f458bb1030d2f346a
nagutabby <62084485+nagutabby@users.noreply.github.com>
2024-08-25 17:54:20 +0900

    第8回と第9回のプログラムを追加する

--- expression_example3.py
+++ expression_example3.py
@@ -6,5 +6,5 @@ assignment1 = AssignmentParseTree(x, two)
 expression1 = MultiplicationParseTree(x, x)
 assignment2 = AssignmentParseTree(x, expression1)
 program1 = SequentialCompositionParseTree(assignment1, assignment2)
-
 print(program1)
+print(program1.interpret({}))
---
+++ expression_example5.py
@@ -0,0 +1,27 @@
+from parse_tree import VariableParseTree, NumberParseTree, AssignmentParseTree, AdditionParseTree, MultiplicationParseTree, RemainderParseTree, SequentialCompositionParseTree
+from list_to_string import l2s
+from vm import VM
+
+x = VariableParseTree('x')
+y = VariableParseTree('y')
+
+two = NumberParseTree(2)
+three = NumberParseTree(3)
+ten = NumberParseTree(10)
+
+assignment1 = AssignmentParseTree(x, ten)
+expression1 = MultiplicationParseTree(x, two)
+assignment2 = AssignmentParseTree(y, expression1)
+expression2 = AdditionParseTree(x, y)
+assignment3 = AssignmentParseTree(x, expression2)
+expression3 = RemainderParseTree(x, three)
+assignment4 = AssignmentParseTree(y, expression3)
+
+program1 = SequentialCompositionParseTree(assignment1, assignment2)
+program2 = SequentialCompositionParseTree(program1, assignment3)
+program3 = SequentialCompositionParseTree(program2, assignment4)
+
+print(program3)
+print(program3.interpret({}))
+print(l2s(program3.compile()))
+print(VM(program3.compile()).run())
